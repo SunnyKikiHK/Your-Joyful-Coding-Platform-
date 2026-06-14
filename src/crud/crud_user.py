60075521b5c549062ca7f7806e75d7f5d05b9acd
max-8811 <maxsong8811@gmail.com>
2026-06-15 01:36:50 +1000

    add CRUD user

---
+++ src/crud/crud_user.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from src.db import models
+from src.schemas import user as user_schemas
+from src.core.security import get_password_hash
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def create_user(db: Session, user: user_schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        email=user.email,
+        username=user.username,
+        hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
